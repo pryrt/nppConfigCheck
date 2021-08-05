023ee38728898e56b049a211f7c4d36f962c65f5
PeterCJ <pryrtcode@pryrt.com>
2021-08-05 07:25:33 -0700

    debug .pl file [ci skip]

---
+++ t/p2u.pl
@@ -0,0 +1,20 @@
+#!perl
+use 5.012; # strict, //
+use warnings;
+use Pod::Usage;
+pod2usage(
+    -message => "message here",
+    -exitval => 2,
+);
+
+=pod
+
+=head1 EXAMPLE
+
+blah
+
+=head1 USAGE
+
+usage here
+
+=cut
