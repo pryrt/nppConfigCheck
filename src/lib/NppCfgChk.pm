01dbaa566425ddb878addd2f00465681407fe3cf
PeterCJ <pryrtcode@pryrt.com>
2021-09-18 16:14:18 -0700

    [ci skip] add the pseudocode outline of what looks like a reasonable algorithm for how to decide which nodes to compare, and do the actual comparison

--- src/lib/NppCfgChk.pm
+++ src/lib/NppCfgChk.pm
@@ -61,4 +61,39 @@ sub grabDirectoryVersion
     1;
 }
 
+sub mergeContents
+{
+    my ($src_contents, $dst_contents, $config) = @_;
+    # the problem with this is that each file has a different set of items, and I'm not sure the best way to handle it;
+    # for example, in config.xml,
+    #   the <GuiConfig> items have unique name attributes, so those would be compared
+    #   but I think on the <FindHistory> and <History> save options from those tabs, but don't have a unique name-like
+    #   attribute to base the comparison on.
+    # Come up with a way to pass in the configuration options:
+    #   have $config = { key => value } , where
+    #       each key is the element type,
+    #       value is the attribute-name of the unique identifier (or `undef` if it's like <History>)
+    #
+    # The algorithm would then be:
+    #   $src = XML::LibXML->load_xml(string => $src_contents)
+    #   $dst = XML::LibXML->load_xml(string => $dst_contents)
+    #   for each $node in $src
+    #   {
+    #       $key = $node->type()                # figure out the type of node
+    #       next unless exists $config->{$key}  # if that type isn't in config, move on to next node
+    #       $id_attr = $config->{$key}          # get the attribute name
+    #       $id_val = $node->hasAttribute($id_attr) ? $node->getAttribute($id_attr) : undef;    # get the value of the attribute, or undef
+    #       $match_expr = defined $id_val ? qq{//$key[\@$id_attr="$id_val"]} : qq{//$key}       # if available, look for the right type of node with id_attr=id_val, else just look for the right type of node
+    #       loop over matching node(s) in destination structure
+    #       {
+    #           loop over attributes from src node
+    #           {
+    #               if srcnode.attr doesn't exist in dstnode, replicate it
+    #           }
+    #       }
+    #   }
+    }
+
+}
+
 1;
